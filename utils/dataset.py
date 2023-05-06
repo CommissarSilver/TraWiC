22b17171ba3abdd1114647455db1cfeeea9027cc
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-05-06 11:39:20 -0400

    added logging

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,5 +1,12 @@
-import os, tqdm, json
+import os, tqdm, json, logging, inspect
 from datasets import load_dataset
+
+logging.basicConfig(
+    filename="runlog.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
 def get_thestack_dataset(
@@ -16,42 +23,75 @@ def get_thestack_dataset(
         save_directory (str, optional): where to store the downloaded scripts. Defaults to os.path.join(os.getcwd(), "data").
         scripts_num (int, optional): number of scripts to download. Defaults to 10**4.
     """
+
+    # for logging purposes. DO NOT CHANGE!
+    frame = inspect.currentframe()
+    frame_info = inspect.getframeinfo(frame)
+
     # we'll use streaming so that it doesn't go and download the entire thing
-    dataset = load_dataset(
-        "bigcode/the-stack", data_dir=f"data/{language}", streaming=True, split="train"
-    )
+    try:
+        dataset = load_dataset(
+            "bigcode/the-stack",
+            data_dir=f"data/{language}",
+            streaming=True,
+            split="train",
+        )
+        logging.info(
+            f"{frame_info.filename} - {frame_info.function} - Succesfully connected to huggingface's TheStack dataset"
+        )
+    except:
+        logging.exception(
+            f"{frame_info.filename} - {frame_info.function} - Error connecting to huggingface's TheStack dataset"
+        )
 
     # create the directory if it doesn't exist
-    if not os.path.exists(os.path.join(save_directory, "the_stack", language)):
-        os.makedirs(os.path.join(save_directory, "the_stack", language))
-    data_dir = os.path.join(save_directory, "the_stack", language)
+    try:
+        if not os.path.exists(os.path.join(save_directory, "the_stack", language)):
+            os.makedirs(os.path.join(save_directory, "the_stack", language))
+        data_dir = os.path.join(save_directory, "the_stack", language)
+        logging.info(
+            f"{frame_info.filename} - {frame_info.function} - Succesfully created the directory for saving the scripts"
+        )
+    except:
+        logging.exception(
+            f"{frame_info.filename} - {frame_info.function} - Error in creating directory for saving the scripts"
+        )
 
     i = 0
     # use tracker to index the hexshas of the stored scripts
     tracker = {}
     # use tqdm to visualize the progress bar
     with tqdm.tqdm(total=scripts_num) as pbar:
-        for dataset_sample in iter(dataset):
-            with open(
-                os.path.join(
-                    os.path.join(data_dir),
-                    f"the_stack_{language}_script_{i}.py",
-                ),
-                "w",
-            ) as f:
-                f.write(dataset_sample["content"])
-                tracker[dataset_sample["hexsha"]] = {
-                    "number": str(i),
-                    "name": f"the_stack_{language}_script_{i}.py",
-                }
+        try:
+            for dataset_sample in iter(dataset):
+                with open(
+                    os.path.join(
+                        os.path.join(data_dir),
+                        f"the_stack_{language}_script_{i}.{dataset_sample['ext']}",
+                    ),
+                    "w",
+                ) as f:
+                    f.write(dataset_sample["content"])
+                    tracker[dataset_sample["hexsha"]] = {
+                        "number": str(i),
+                        "name": f"the_stack_{language}_script_{i}.{dataset_sample['ext']}",
+                    }
 
-            i += 1
+                i += 1
 
-            pbar.update(1)
+                pbar.update(1)
 
-            if i == scripts_num:
-                json.dump(tracker, open(os.path.join(data_dir, "index.json"), "w"))
-                break
+                if i == scripts_num:
+                    json.dump(tracker, open(os.path.join(data_dir, "index.json"), "w"))
+                    break
+            logging.info(
+                f"{frame_info.filename} - {frame_info.function} - Succesfully downloaded and stored the scripts"
+            )
+        except:
+            logging.exception(
+                f"{frame_info.filename} - {frame_info.function} - Error in dowloading/storing the scripts"
+            )
 
 
-get_thestack_dataset()
+if __name__=="__main__":
+    get_thestack_dataset()
