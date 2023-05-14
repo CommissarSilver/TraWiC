6522db4e211891776b1a8a4b74614d5d1dbd2fee
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-05-14 11:59:09 -0400

    changes: - changed variable regex to only include var names - the script contents are part of the checker object now - separate_scripts function for producing prefix and suffix - prepare_inputs_for_infill method for generating inputs to model

--- src/checker/checker.py
+++ src/checker/checker.py
@@ -5,7 +5,7 @@ sys.path.insert(0, parent_dir)
 import logger_utils
 
 logger = logger_utils.CustomLogger(
-    "checker.log", create_directory=False, log_level=logging.INFO
+    "checker.log", create_directory=True, log_level=logging.DEBUG
 )
 
 
@@ -13,6 +13,7 @@ class Checker:
     def __init__(self, input_path: str) -> None:
         if input_path.endswith(".py"):
             self.input_path = input_path
+            self.original_input = open(self.input_path, "r").read()
         else:
             raise NotImplementedError
         self.processed_input = None
@@ -29,26 +30,27 @@ class Checker:
         the results are stored in the `processed_input` attribute of the class
 
         """
-        input = open(self.input_path, "r").read()
 
         # use regex to extract the mentioned items
-        docstrings_iter = re.finditer(r'"""[\s\S]*?"""', input)
-        comments_iter = re.finditer(r"#.*", input)
+        docstrings_iter = re.finditer(r'"""[\s\S]*?"""', self.original_input)
+        comments_iter = re.finditer(r"#.*", self.original_input)
         function_names_iter = re.finditer(
-            r"def\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*\((.*)\)", input
+            r"def\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*\((.*)\)", self.original_input
         )
         class_names_iter = re.finditer(
-            r"class\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*(?:\((.*?)\))?", input
+            r"class\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*(?:\((.*?)\))?", self.original_input
         )
-        variable_names_iter = re.finditer(r"([a-zA-Z_][a-zA-Z0-9_]*)\s*=", input)
-        strings_iter = re.finditer(r"\".*?\"|'.*?'", input)
+        variable_names_iter = re.finditer(
+            r"([a-zA-Z_][a-zA-Z0-9_]*)\s*=", self.original_input
+        )
+        strings_iter = re.finditer(r"\".*?\"|'.*?'", self.original_input)
 
         # for each of the items, we need to store their value and their line numbers
         try:
             docstrings = {
                 (
-                    input.count("\n", 0, match.start()) + 1,
-                    input.count("\n", 0, match.end()) + 1,
+                    self.original_input.count("\n", 0, match.start()) + 1,
+                    self.original_input.count("\n", 0, match.end()) + 1,
                 ): match.group()
                 for match in docstrings_iter
             }
@@ -60,8 +62,8 @@ class Checker:
         try:
             comments = {
                 (
-                    input.count("\n", 0, match.start()) + 1,
-                    input.count("\n", 0, match.end()) + 1,
+                    self.original_input.count("\n", 0, match.start()) + 1,
+                    self.original_input.count("\n", 0, match.end()) + 1,
                 ): match.group()
                 for match in comments_iter
             }
@@ -73,8 +75,8 @@ class Checker:
         try:
             function_names = {
                 (
-                    input.count("\n", 0, match.start()) + 1,
-                    input.count("\n", 0, match.end()) + 1,
+                    self.original_input.count("\n", 0, match.start()) + 1,
+                    self.original_input.count("\n", 0, match.end()) + 1,
                 ): (match.group(1), match.group(2))
                 for match in function_names_iter
             }
@@ -86,8 +88,8 @@ class Checker:
         try:
             class_names = {
                 (
-                    input.count("\n", 0, match.start()) + 1,
-                    input.count("\n", 0, match.end()) + 1,
+                    self.original_input.count("\n", 0, match.start()) + 1,
+                    self.original_input.count("\n", 0, match.end()) + 1,
                 ): (match.group(1), match.group(2))
                 for match in class_names_iter
             }
@@ -99,9 +101,9 @@ class Checker:
         try:
             variable_names = {
                 (
-                    input.count("\n", 0, match.start()) + 1,
-                    input.count("\n", 0, match.end()) + 1,
-                ): match.group()
+                    self.original_input.count("\n", 0, match.start()) + 1,
+                    self.original_input.count("\n", 0, match.end()) + 1,
+                ): match.group(1)
                 for match in variable_names_iter
             }
             logger.debug(f"variable_names: {variable_names}")
@@ -112,8 +114,8 @@ class Checker:
         try:
             strings = {
                 (
-                    input.count("\n", 0, match.start()) + 1,
-                    input.count("\n", 0, match.end()) + 1,
+                    self.original_input.count("\n", 0, match.start()) + 1,
+                    self.original_input.count("\n", 0, match.end()) + 1,
                 ): match.group()
                 for match in strings_iter
             }
@@ -133,10 +135,74 @@ class Checker:
             "strings": strings,
         }
 
+    @staticmethod
+    def separate_script(script_text, word, line_number):
+        lines = script_text.split("\n")
+        prefix = "\n".join(
+            lines[: line_number - 1]
+        )  # Prefix contains lines before the specified line number
+        suffix = "\n".join(
+            lines[line_number - 1 :]
+        )  # Suffix contains lines from the specified line number onwards
+
+        # Find the index of the word in the suffix and where it finishes
+        word_index = suffix.find(word)
+        word_end = word_index + len(word)
+
+        prefix += suffix[:word_index]
+        suffix = suffix[word_end:]
+
+        return prefix, suffix
+
+    def prepare_inputs_for_infill(self, level):
+        candidates = []
+
+        if self.processed_input[level] != None:
+            model_input_candidates = self.processed_input[level]
+        else:
+            raise "there are no candidates for this level"
+
+        for key, item in model_input_candidates.items():
+            if level in ("function_names", "class_names"):
+                # for function and class names, we need to separate the script into prefix and suffix
+                prefix, suffix = Checker.separate_script(
+                    script_text=self.original_input, word=item[0], line_number=key[0]
+                )
+                candidates.append(
+                    {
+                        "infill": item[0],
+                        "line": key[0],
+                        "prefix": prefix,
+                        "suffix": suffix,
+                    }
+                )
+                # if the function has arguments, or the class inherits from another class, we need to add them to the infill as well
+                if item[1] != None:
+                    prefix, suffix = Checker.separate_script(
+                        script_text=self.original_input, word=item[1], line_number=key[0]
+                    )
+                    candidates.append(
+                        {
+                            "infill": item[1],
+                            "line": key[0],
+                            "prefix": prefix,
+                            "suffix": suffix,
+                        }
+                    )
+            elif level in ("strings", "variable_names"):
+                prefix, suffix = Checker.separate_script(
+                    script_text=self.original_input, word=item, line_number=key[0]
+                )
+                candidates.append({"infill": item, "prefix": prefix, "suffix": suffix})
+            elif level in ("docstrings", "comments"):
+                raise "not implemented yet"
+        return candidates
+
 
 if __name__ == "__main__":
     checker = Checker(
         "/Users/ahura/Nexus/TWMC/data/the_stack/python/the_stack_python_script_0.py"
     )
     checker.prepare_input()
-    print(checker.processed_input)
+    x = checker.prepare_inputs_for_infill("strings")
+    print(x)
