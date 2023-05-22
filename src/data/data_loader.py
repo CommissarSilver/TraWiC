d18e9ce2bc60bdce0d548b9be9edf238ed506d1f
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-05-22 19:21:53 -0400

    changes to data_loader: - documentation

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -4,17 +4,45 @@ from torch.utils.data import DataLoader, Dataset
 
 
 class CodeInfillDataset(Dataset):
-    def __init__(self, dataset_path, code_language) -> None:
+    """
+    A PyTorch dataset for code infilling tasks.
+
+    Args:
+        dataset_path (str): The path to the directory containing the dataset files.
+        code_language (str): The programming language of the code files in the dataset.
+
+    Attributes:
+        dataset_path (str): The path to the directory containing the dataset files.
+        code_language (str): The programming language of the code files in the dataset.
+        code_suffix (str): The file suffix for code files in the dataset.
+    """
+
+    def __init__(self, dataset_path: str, code_language: str) -> None:
         self.dataset_path = dataset_path
         self.code_language = code_language
         self.code_suffix = ".py" if self.code_language == "python" else ".js"
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """
+        Returns the number of code files in the dataset.
+
+        Returns:
+            int: The number of code files in the dataset.
+        """
         return len(
             [f for f in os.listdir(self.dataset_path) if f.endswith(self.code_suffix)]
         )
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> str:
+        """
+        Returns the input code string at the given index.
+
+        Args:
+            index (int): The index of the input code string to return.
+
+        Returns:
+            str: The input code string at the given index.
+        """
         try:
             file_path = os.path.join(
                 self.dataset_path,
@@ -22,5 +50,6 @@ class CodeInfillDataset(Dataset):
             )
             input_code_string = open(file_path, "r").read()
             return input_code_string
+
         except FileNotFoundError:
-            pass
+            return ""
