628f48d31c60851fb60cde5b8e7df9498e6f7ae0
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-05-23 16:06:37 -0400

    First version for running on Graham

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
-import os, logging, logging.config, yaml
-
-from data import get_thestack_dataset
-from utils import word_count_directory
-from models import SantaCoder
+import os, logging, logging.config, yaml, torch, argparse, random, json
 from checker import Checker
+from models import SantaCoder
+
 
 # load logging configuration
 with open(os.path.join(os.getcwd(), "src", "logging_config.yaml"), "r") as f:
@@ -11,95 +9,76 @@ with open(os.path.join(os.getcwd(), "src", "logging_config.yaml"), "r") as f:
 
 logging.config.dictConfig(config)
 
-if __name__ == "__main__":
-    # get_thestack_dataset(
-    #     language="python",
-    #     save_directory=os.path.join(os.getcwd(), "data"),
-    #     scripts_num=100,
-    # )
-    # word_count_directory(
-    #     directory_path=os.path.join(os.getcwd(), "data", "the_stack", "python"),
-    #     script_suffix=".py",
-    # )
+parser = argparse.ArgumentParser(description="Trained Without My Consent")
+parser.add_argument(
+    "--language",
+    type=str,
+    default="py",
+    help="language of the code",
+)
+parser.add_argument(
+    "--dataset_path",
+    type=str,
+    default="data/the_stack/python",
+    help="path to the dataset",
+)
+parser.add_argument(
+    "--batch_size",
+    type=int,
+    default=32,
+    help="batch size",
+)
+args = parser.parse_args()
 
-    test_input = Checker(
-        "/Users/ahura/Nexus/TWMC/data/the_stack/python/the_stack_python_script_0.py"
-    )
+model = SantaCoder()
 
-    candidate_function_inputs = test_input.prepare_inputs_for_infill(
-        level="function_names"
-    )
-    candidate_variable_inputs = test_input.prepare_inputs_for_infill(
-        level="variable_names"
-    )
-    candidate_class_inputs = test_input.prepare_inputs_for_infill(level="class_names")
-    candidate_comment_inputs = test_input.prepare_inputs_for_infill(level="comments")
-    candidate_docstring_inputs = test_input.prepare_inputs_for_infill(level="docstrings")
-    candidate_string_inputs = test_input.prepare_inputs_for_infill(level="strings")
+dataset_files_path = [
+    os.path.join(os.getcwd(), args.dataset_path, file)
+    for file in os.listdir(os.path.join(os.getcwd(), args.dataset_path))
+    if file.endswith(args.language)
+]
 
-    candidate_inputs = {
-        "function_names": {
-            "inputs": candidate_function_inputs,
-            "similarity_metric": "exact",
-        },
-        "variable_names": {
-            "inputs": candidate_variable_inputs,
-            "similarity_metric": "exact",
-        },
-        "class_names": {
-            "inputs": candidate_class_inputs,
-            "similarity_metric": "exact",
-        },
-        "comments": {
-            "inputs": candidate_comment_inputs,
-            "similarity_metric": "fuzzy",
-        },
-        "docstrings": {
-            "inputs": candidate_docstring_inputs,
-            "similarity_metric": "fuzzy",
-        },
-        "strings": {
-            "inputs": candidate_string_inputs,
-            "similarity_metric": "fuzzy",
-        },
-    }
-    model = SantaCoder()
+for file_path in dataset_files_path:
+    # select randomly from dataset_files_path
+    file_checker = Checker(file_path)
 
+    model_inputs = [
+        file_checker.prepare_inputs_for_infill(level=i)
+        for i in [
+            "function_names",
+            "variable_names",
+            "class_names",
+            "comments",
+            "docstrings",
+            "strings",
+        ]
+    ]
+
+    model_inputs = [input for sublist in model_inputs for input in sublist]
     results = []
-    for candidate_input_level, candidate_input in candidate_inputs.items():
-        inputs = candidate_input["inputs"]
+    for candidate_input in model_inputs[:2]:
+        model_output = model.infill(
+            (candidate_input["prefix"], candidate_input["suffix"])
+        )
 
-        model_output = model.infill((inputs["prefix"], inputs["suffix"]))
-
-        result = test_input.check_similarity(
+        result = file_checker.check_similarity(
             model_output,
             candidate_input,
-            similiarity_metric=candidate_input["similarity_metric"],
+            similiarity_metric="exact"
+            if candidate_input["level"]
+            in ["function_names", "variable_names", "class_names"]
+            else "fuzzy",
         )
-        results.append(result)
-    # save the results to a json file
-    import json
+
+        results.append(
+            {
+                "file_path": file_path,
+                "similarity_metric": "exact"
+                if candidate_input["level"]
+                in ["function_names", "variable_names", "class_names"]
+                else "fuzzy",
+                "result": result,
+            }
+        )
 
     json.dump(results, open(os.path.join(os.getcwd(), "results.json"), "w"))
-    # if False:
-    #     if candidate_input["infill"] in model_output:
-    #         print(
-    #             "\033[92m"
-    #             + candidate_input["prefix"]
-    #             + "\033[93m"
-    #             + model_output
-    #             + "\033[92m"
-    #             + candidate_input["suffix"]
-    #         )
-    #         print(
-    #             model_output.count(
-    #                 "\n", 0, model_output.find(candidate_input["infill"])
-    #             )
-    #         )
-
-    # j = model.infill((candidate_inputs[0]["prefix"], candidate_inputs[0]["suffix"]))
-    # search for the infill in the original script
-    # if candidate_inputs[0]["infill"] in j:
-    # print("found it")
-    # print("\033[92m" + candidate_inputs[0]["prefix"] + "\033[93m" + j + "\033[92m" + candidate_inputs[0]["suffix"])
-    print("hi")
