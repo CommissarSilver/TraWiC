b660c53ef3c51b9b907517c23e93ffa27a780d67
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-06-18 17:38:23 -0400

    MODIFIED src/main.py: (RETURN TO THIS COMMIT IF THINGS GO WRONG)
    - changed entire training loop
    - now interface with the model is done through a function call

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ parser.add_argument(
     type=str,
     default="py",
     help="language of the code",
-)
+)  # programming language
 parser.add_argument(
     "--dataset_path",
     type=str,
@@ -28,28 +28,17 @@ parser.add_argument(
     type=int,
     default=32,
     help="batch size",
-)
+)  # batch size
+parser.add_argument("--model", type=str, default="santa_coder", help="model to use")
+
 args = parser.parse_args()
 
-model = SantaCoder()
-
-dataset_files_path = [
-    os.path.join(os.getcwd(), args.dataset_path, file)
-    for file in os.listdir(os.path.join(os.getcwd(), args.dataset_path))
-    if file.endswith(args.language)
-]
-results_so_far = pd.read_csv(os.path.join(os.getcwd(), "src", "trained_on.csv"))
-resluts_so_far_names = results_so_far["file_name"].tolist()
-
-dataset_files_path = [
-    file for file in dataset_files_path if file.split("/")[-1] not in resluts_so_far_names
-]
+model = SantaCoder() if args.model == "santa_coder" else None
 
 
-for file_path in dataset_files_path:
-    # select randomly from dataset_files_path
+def get_model_output(file_path):
+    results = []
     file_checker = Checker(file_path)
-
     model_inputs = [
         file_checker.prepare_inputs_for_infill(level=i)
         for i in [
@@ -61,55 +50,67 @@ for file_path in dataset_files_path:
             "strings",
         ]
     ]
-    # print file path in red
-    print("\033[91m" + file_path.split("/")[-1] + "\033[0m")
     model_inputs = [input for sublist in model_inputs for input in sublist]
-    if model_inputs == []:
-        continue
-    for candidate_input in tqdm(model_inputs):
-        results = []
 
+    if model_inputs == []:
+        return None
+    for candidate_input in tqdm(model_inputs):
         model_output = model.infill(
             (candidate_input["prefix"], candidate_input["suffix"])
         )
         if model_output == "too_many_tokens":
-            print(
-                "\033[91m"
-                + file_path.split("/")[-1]
-                + " has too many tokens - skipping"
-                + "\033[0m"
-            )
             f = open(os.path.join(os.getcwd(), "too_many_tokens.txt"), "a")
-            f.write(file_path + "\n")
-            break
-        try:
-            result = file_checker.check_similarity(
-                model_output,
-                candidate_input,
-                similiarity_metric="exact"
-                if candidate_input["level"]
-                in ["function_names", "variable_names", "class_names"]
-                else "fuzzy",
-            )
-            results.append(
-                {
-                    "file_path": file_path,
-                    "level": candidate_input["level"],
-                    "similarity_metric": "exact"
+            f.write(file_path.split("/")[-1] + "\n")
+            return None
+        else:
+            try:
+                result = file_checker.check_similarity(
+                    model_output,
+                    candidate_input,
+                    similiarity_metric="exact"
                     if candidate_input["level"]
                     in ["function_names", "variable_names", "class_names"]
                     else "fuzzy",
-                    "result": result,
-                    "similarity_objective": candidate_input["infill"],
-                    "model_output": model_output,
-                }
-            )
-        except Exception as e:
-            logging.error(e)
+                )
+                results.append(
+                    {
+                        "file_path": file_path,
+                        "level": candidate_input["level"],
+                        "similarity_metric": "exact"
+                        if candidate_input["level"]
+                        in ["function_names", "variable_names", "class_names"]
+                        else "fuzzy",
+                        "result": result,
+                        "similarity_objective": candidate_input["infill"],
+                        "model_output": model_output,
+                    }
+                )
+            except Exception as e:
+                logging.error(e)
+                return None
+    with open(os.path.join(os.getcwd(), "results.jsonl"), "a") as f:
+        json_results = json.dumps(results)
+        f.write(json_results)
+        f.write("\n")
+    # return results
 
-        with open(os.path.join(os.getcwd(), "results.jsonl"), "a") as f:
-            for result in results:
-                json_results = json.dumps(results)
-                f.write(json_results)
-                f.write("\n")
-    # add to results.json don't overwrite
+
+if __name__ == "__main__":
+    dataset_files_path = [
+        os.path.join(os.getcwd(), args.dataset_path, file)
+        for file in os.listdir(os.path.join(os.getcwd(), args.dataset_path))
+        if file.endswith(args.language)
+    ]
+    results_so_far = pd.read_csv(os.path.join(os.getcwd(), "src", "trained_on.csv"))
+    resluts_so_far_names = results_so_far["file_name"].tolist()
+
+    dataset_files_path = [
+        file
+        for file in dataset_files_path
+        if file.split("/")[-1] not in resluts_so_far_names
+    ]
+
+    for file_path in dataset_files_path:
+        results = []
+        print("\033[91m" + file_path.split("/")[-1] + "\033[0m")
+        result = get_model_output(file_path)
