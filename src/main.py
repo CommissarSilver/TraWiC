079615497b216d02abbe1fa54bb86b1ea2011492
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-06-17 23:18:53 -0400

    MODIFIED src/main.py:
    - No more calling the model 5 times for each candidate
    - added a file for storing the files with too many tokens.
    - instead of continuing if there are too many tokens, we go to the nexxt file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os, logging, logging.config, yaml, torch, argparse, random, json
+from tqdm import tqdm
 from checker import Checker
 from models import SantaCoder
 import pandas as pd
@@ -44,6 +45,7 @@ dataset_files_path = [
     file for file in dataset_files_path if file.split("/")[-1] not in resluts_so_far_names
 ]
 
+
 for file_path in dataset_files_path:
     # select randomly from dataset_files_path
     file_checker = Checker(file_path)
@@ -64,41 +66,42 @@ for file_path in dataset_files_path:
     model_inputs = [input for sublist in model_inputs for input in sublist]
     if model_inputs == []:
         continue
-    for candidate_input in model_inputs:
+    for candidate_input in tqdm(model_inputs):
         results = []
-        for i in range(5):
-            model_output = model.infill(
-                (candidate_input["prefix"], candidate_input["suffix"])
+
+        model_output = model.infill(
+            (candidate_input["prefix"], candidate_input["suffix"])
+        )
+        if model_output == "too_many_tokens":
+            print("\033[91m" + file_path + "has too many tokens - skipping" + "\033[0m")
+            f = open(os.path.join(os.getcwd(), "too_many_tokens.txt"), "a")
+            f.write(file_path + "\n")
+            break
+        try:
+            result = file_checker.check_similarity(
+                model_output,
+                candidate_input,
+                similiarity_metric="exact"
+                if candidate_input["level"]
+                in ["function_names", "variable_names", "class_names"]
+                else "fuzzy",
             )
-            if model_output == "too_many_tokens":
-                print(
-                    "\033[91m" + file_path + "has toom any tokens - skipping" + "\033[0m"
-                )
-                continue
-            try:
-                result = file_checker.check_similarity(
-                    model_output,
-                    candidate_input,
-                    similiarity_metric="exact"
+            results.append(
+                {
+                    "file_path": file_path,
+                    "level": candidate_input["level"],
+                    "similarity_metric": "exact"
                     if candidate_input["level"]
                     in ["function_names", "variable_names", "class_names"]
                     else "fuzzy",
-                )
-                results.append(
-                    {
-                        "file_path": file_path,
-                        "level": candidate_input["level"],
-                        "similarity_metric": "exact"
-                        if candidate_input["level"]
-                        in ["function_names", "variable_names", "class_names"]
-                        else "fuzzy",
-                        "result": result,
-                        "similarity_objective": candidate_input["infill"],
-                        "model_output": model_output,
-                    }
-                )
-            except Exception as e:
-                logging.error(e)
+                    "result": result,
+                    "similarity_objective": candidate_input["infill"],
+                    "model_output": model_output,
+                }
+            )
+        except Exception as e:
+            logging.error(e)
+
         with open(os.path.join(os.getcwd(), "results.jsonl"), "a") as f:
             for result in results:
                 json_results = json.dumps(results)
