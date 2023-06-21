3d8bbdb9d576ddeadf2a2e289eab475a5ca94f41
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-06-21 12:47:14 -0400

    UPDATED inspector_train.py:
    - added visualization of correlation matrix, feature importance and one of the trees in the random forest
    - let go of the linear model for decision making. only using random forest now

--- src/inspector_train.py
+++ src/inspector_train.py
@@ -1,12 +1,32 @@
 from models import InspectorModel, InspectorModelRF
 import torch
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
-ds = pd.read_csv("/Users/ahura/Nexus/TWMC/lm_dataset.csv")
-ds.drop(
+# read the list of csv files, and combine them all into a single dataframe
+datasets = [
+    "/Users/ahura/Nexus/TWMC/Run 02_processed_dataset.csv",
+    "/Users/ahura/Nexus/TWMC/Run 03_processed_dataset.csv",
+    "/Users/ahura/Nexus/TWMC/Run 04_processed_dataset.csv",
+    "/Users/ahura/Nexus/TWMC/Run 05_processed_dataset.csv",
+    "/Users/ahura/Nexus/TWMC/Run 06_processed_dataset.csv",
+    "/Users/ahura/Nexus/TWMC/Run 07_processed_dataset.csv",
+    "/Users/ahura/Nexus/TWMC/Run 08_processed_dataset.csv",
+    "/Users/ahura/Nexus/TWMC/Run 09_processed_dataset.csv",
+    "/Users/ahura/Nexus/TWMC/Run 10_processed_dataset.csv",
+]
+datasets = [pd.read_csv(path) for path in datasets]
+combined_ds = pd.concat(datasets)
+
+print(combined_ds["trained_on"].value_counts())
+
+combined_ds.drop(
     columns=[
-        "file_name",
         "class_nums_total",
         "function_nums_total",
         "variable_nums_total",
@@ -16,63 +36,58 @@ ds.drop(
     ],
     inplace=True,
 )
-#### Linear Model ####
-#! Unstable. may have to opt out to using ensemble models
-# convert the dataframe to a tensor, the last column is the label
-x = torch.tensor(ds.iloc[:, 1:].values, dtype=torch.float32)
-y = torch.tensor(ds.iloc[:, -1].values, dtype=torch.float32)
-# break these down into batches of 32
+combined_ds.to_csv("combined_ds.csv", index=False)
 
-lr = 0.001
-epochs = 100
-early_stopping_patience = 15
-best_loss = float("inf")
-patience_counter = 0
+# Split the dataset into training and testing datasets
+train_ds, test_ds = train_test_split(combined_ds, test_size=0.2, random_state=42)
+# Split the testing dataset into testing and validation datasets
+test_ds, val_ds = train_test_split(test_ds, test_size=0.5, random_state=42)
+# drop the index column
+train_ds.drop(columns=["Unnamed: 0"], inplace=True)
+test_ds.drop(columns=["Unnamed: 0"], inplace=True)
+val_ds.drop(columns=["Unnamed: 0"], inplace=True)
 
-model = InspectorModel(6)
-# categorical cross entropy loss
-loss = torch.nn.BCEWithLogitsLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+##### Random Forest ####
+x, y = train_ds.iloc[:, 1:].values, train_ds.iloc[:, -1].values
+print(f"Features shape: {x.shape}")
+print(f"Target shape: {y.shape}")
+print(f"Features Snippet: {x[:5]}")
+print(f"Target Snippet: {y[:5]}")
 
-for epoch in range(epochs):
-    # forward pass
-    y_pred = model(x)
-    # compute loss
-    l = loss(y_pred, y.unsqueeze(1))
-    # backward pass
-    l.backward()
-    # update parameters
-    optimizer.step()
-    # zero the gradients
-    optimizer.zero_grad()
-
-    if l.item() < best_loss:
-        best_loss = l.item()
-        patience_counter = 0
-    else:
-        patience_counter += 1
-        if patience_counter >= early_stopping_patience:
-            print(f"Early stopping at epoch {epoch+1}")
-            break
-
-    # Print accuracy every ten epochs in green and loss in red
-    if (epoch + 1) % 10 == 0:
-        print(
-            f"Epoch {epoch+1}:",
-            "\033[32mAccuracy:",
-            (y_pred.round() == y.unsqueeze(1)).sum().item() / len(y),
-            "\033[0m",
-            "\033[34mLoss:",
-            l.item(),
-            "\033[0m",
-        )
-#### Linear Model ####
-
-#### Random Forest ####
-x, y = ds.iloc[:, 1:].values, ds.iloc[:, -1].values
 clf = RandomForestClassifier(n_estimators=100, max_depth=100, random_state=0)
 clf.fit(x, y)
-from sklearn.tree import export_graphviz
+##### Feature Importance ####
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111)
+ax.barh(train_ds.columns[:-1], clf.feature_importances_)
+plt.savefig("feature_importance.png", dpi=300)
 
-export_graphviz(clf.estimators_[0], out_file="tree.dot", feature_names=ds.columns[:-1])
-#### Random Forest ####
+#### Correlation Matrix ####
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111)
+sns.heatmap(train_ds.corr(), annot=True, fmt=".2f", ax=ax)
+plt.savefig("correlation_matrix.png", dpi=300)
+
+# plot the first decision tree
+plt.figure(figsize=(40, 40))
+tree.plot_tree(
+    clf.estimators_[0],
+    feature_names=train_ds.columns[:-1],
+    class_names=["0", "1"],
+    filled=True,
+    fontsize=10,
+)
+plt.savefig("decision_tree.png", dpi=300)
+# print the number of 1s and 0s in the dataset
+print("Number of 1s and 0s in the train dataset:", train_ds["trained_on"].value_counts())
+print("Number of 1s and 0s in the test dataset:", test_ds["trained_on"].value_counts())
+# create a confusion matrix and print it
+from sklearn.metrics import confusion_matrix
+
+print("Confusion matrix:")
+print(
+    confusion_matrix(test_ds.iloc[:, -1].values, clf.predict(test_ds.iloc[:, 1:].values))
+)
+# print the accuracy
+accuracy = clf.score(test_ds.iloc[:, 1:].values, test_ds.iloc[:, -1].values)
+print("Accuracy:", accuracy)
