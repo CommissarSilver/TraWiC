a52a104977ddffccefe816c80df0c32d61734ddc
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-06-29 10:23:19 -0400

    MODIFIED to src/main_block.py:
    - Added sending the suffix to the model to get as many tookens as exists in the suffix out of the model

--- src/main_block.py
+++ src/main_block.py
@@ -1,9 +1,12 @@
-import os, logging, logging.config, yaml, torch, argparse, random, json
+import os, logging, logging.config, yaml, torch, argparse, random, json, warnings
 from tqdm import tqdm
 from checker import Checker, CheckerBlock
 from models import SantaCoder, SantaCoderBlock
 import pandas as pd
 
+
+# Disable all warnings
+warnings.filterwarnings("ignore")
 # load logging configuration
 with open(os.path.join(os.getcwd(), "src", "logging_config.yaml"), "r") as f:
     config = yaml.safe_load(f.read())
@@ -33,13 +36,14 @@ def get_model_output_inspector(file_path):
     model_inputs = file_checker.prepare_inputs_for_prediction()
 
     for candidate_input in tqdm(model_inputs):
-        model_output = model.predict(candidate_input["prefix"])
+        model_output = model.predict(candidate_input["prefix"], candidate_input["suffix"])
         candidate_input["model_output"] = model_output
-        results.append(candidate_input)       
+        results.append(candidate_input)
     with open(os.path.join(os.getcwd(), "results_block.jsonl"), "a") as f:
         json_results = json.dumps(results)
         f.write(json_results)
         f.write("\n")
+
 
 if __name__ == "__main__":
     dataset_files_path = [
@@ -47,7 +51,15 @@ if __name__ == "__main__":
         for file in os.listdir(os.path.join(os.getcwd(), args.dataset_path))
         if file.endswith(args.language)
     ]
-    for file_path in dataset_files_path:
+
+    results_so_far = pd.read_csv(os.path.join(os.getcwd(), "src", "test_ds.csv"))
+    resluts_so_far_names = results_so_far["file_name"].tolist()
+
+    test_ds_path = [
+        file for file in dataset_files_path if file.split("/")[-1] in resluts_so_far_names
+    ]
+
+    for file_path in test_ds_path:
         results = []
         print("\033[91m" + file_path.split("/")[-1] + "\033[0m")
         result = get_model_output_inspector(file_path)
