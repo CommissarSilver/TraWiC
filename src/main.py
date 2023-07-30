a751a00fac4a8b52bf940e9b9f73a664b477cef6
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-07-30 12:46:01 -0400

    TEMP changes to src/main.py

--- src/main.py
+++ src/main.py
@@ -29,7 +29,19 @@ parser.add_argument(
     default=32,
     help="batch size",
 )  # batch size
-parser.add_argument("--model", type=str, default="santa_coder", help="model to use")
+parser.add_argument(
+    "--model",
+    type=str,
+    default="santa_coder",
+    help="model to use",
+)
+parser.add_argument(
+    "--sorted",
+    type=bool,
+    default=False,
+    help="sort the dataset",
+)
+parser.add_argument("--run_num", type=str, default="0")
 
 args = parser.parse_args()
 
@@ -56,10 +68,12 @@ def get_model_output(file_path):
         return None
     for candidate_input in tqdm(model_inputs):
         model_output = model.infill(
-            (candidate_input['infill'],
-             candidate_input["prefix"], 
-             candidate_input["suffix"],
-             candidate_input["level"])
+            (
+                candidate_input["infill"],
+                candidate_input["prefix"],
+                candidate_input["suffix"],
+                candidate_input["level"],
+            )
         )
         if model_output == "too_many_tokens":
             f = open(os.path.join(os.getcwd(), "too_many_tokens.txt"), "a")
@@ -98,14 +112,51 @@ def get_model_output(file_path):
 
 
 if __name__ == "__main__":
-    dataset_files=[]
-    for dirpath, dirnames, filenames in os.walk(args.dataset_path):
+    print(args.sorted)
+    print(type(args.run_num))
+    print("Available devices: ", torch.cuda.device_count())
+    if torch.cuda.is_available():
+        logging.info(f"GPU is available. Running on {torch.cuda.get_device_name(0)}")
+    else:
+        logging.info("GPU is not available. Running on CPU")
+
+    # dataset_files = []
+    # for dirpath, dirnames, filenames in os.walk(args.dataset_path):
+    #     python_files = [file for file in filenames if file.endswith(".py")]
+    #     if python_files:
+    #         dataset_files.extend(
+    #             [os.path.join(os.getcwd(), dirpath, file) for file in python_files]
+    #         )
+    dataset_files = []
+    for dirpath, dirnames, filenames in os.walk("/home/vamaj/scratch/jenova/data"):
         python_files = [file for file in filenames if file.endswith(".py")]
         if python_files:
-            dataset_files.extend([os.path.join(os.getcwd(),dirpath, file) for file in python_files])
-    
-    for file_path in dataset_files:
-        results = []
-        print("\033[91m" + file_path + "\033[0m")
-        result = get_model_output(file_path)
+            dataset_files.extend(
+                [
+                    os.path.join("/scratch/vamaj/jenova/data", dirpath, file)
+                    for file in python_files
+                ]
+            )
+        # already_processed = open(
+        #     os.path.join(os.getcwd(), "generated.txt"), "r"
+        # ).readlines()  # read already processed files
+    already_processed = open(
+        ("/home/vamaj/scratch/jenova/generated.txt"),
+        "r",
+    ).readlines()
 
+    if already_processed:
+        already_processed = [i.replace("\n", "") for i in already_processed]
+        already_processed = (
+            sorted(already_processed)
+            if args.sorted
+            else sorted(already_processed, reverse=True)
+        )
+    # for file_path in dataset_files:
+    #     if file_path in already_processed:
+    for file_path in dataset_files:
+        # x=file_path.split('/data',1)[1]
+        if file_path.split("/data", 1)[1] in already_processed:
+            results = []
+            print("\033[91m" + file_path + "\033[0m")
+            result = get_model_output(file_path)
