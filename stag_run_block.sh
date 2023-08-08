9107903b6020147c22896a700973f10880cde41a
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-08-08 09:21:02 -0400

    MODIFIED stag_run_block.sh:
    - added more conditions for run checking

--- stag_run_block.sh
+++ stag_run_block.sh
@@ -15,11 +15,21 @@ sorted="True"
 source /home/vamaj/twmc/bin/activate
 cd /home/vamaj/scratch/TWMC
 
-# Loop to keep running the script if exit code is 1
+# Loop to keep running the script if exit code is 2
 while true; do
     python /home/vamaj/scratch/TWMC/src/main_block.py --run_num "$run_num" --working_dir "$working_dir"
-    if [ $? -ne 1 ]; then
+    exit_code=$?
+
+    if [ $exit_code -eq 0 ]; then
+        echo "Script executed successfully."
+        break
+    elif [ $exit_code -eq 1 ]; then
+        echo "Received exit code 1. Performing another action..."
+        break
+    elif [ $exit_code -eq 2 ]; then
+        echo "CUDA error encountered, rerunning script..."
+    else
+        echo "Unknown exit code $exit_code. Exiting the loop."
         break
     fi
-    echo "CUDA error encountered, rerunning script..."
 done
