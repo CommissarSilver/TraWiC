078f44730bcc69ebc52c4beddf1016504c8746f4
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-08-13 10:06:42 -0400

    MODIFIED utils/block_code_builder:
    - changed how the block code's were created in teir directories in order to make it more organized
    - added removing any left white spaces in te model's outputs since it makes NiCAD unable to prase the file

--- src/utils/block_code_builder.py
+++ src/utils/block_code_builder.py
@@ -1,7 +1,9 @@
 import json, tqdm, os
 
 BLOCKS_PATH = "/Users/ahura/Nexus/TWMC/src/blocks/"
-results_jsonl = open("/Users/ahura/Nexus/TWMC/Runs/BlocksRun2/results_block.jsonl", "r")
+results_jsonl = open(
+    "/Users/ahura/Nexus/TWMC/run_results/BlocksRun2/results_block_2.jsonl", "r"
+)
 results_data = [line for line in results_jsonl]
 
 error_count = 0
@@ -10,24 +12,26 @@ for data in tqdm.tqdm(results_data):
         data = json.loads(data)
         for entry_num, entry in enumerate(data):
             file_dir = entry["file_path"].split("/data", 1)[1].split("/")[1]
+            # filename is the part that end in .py
+            file_name = entry["file_path"].split("/")[-1]
+            if not os.path.exists(
+                os.path.join(BLOCKS_PATH, file_dir, file_name.replace(".py", ""))
+            ):
+                os.makedirs(
+                    os.path.join(BLOCKS_PATH, file_dir, file_name.replace(".py", ""))
+                )
 
-            if not os.path.exists(BLOCKS_PATH + file_dir):
-                os.makedirs(BLOCKS_PATH + file_dir)
-
-            save_path = (
-                BLOCKS_PATH
-                + file_dir
-                + "/"
-                + f"func_num_{entry_num}"
-                + entry["file_path"].split("/")[-1]
+            save_path = os.path.join(
+                BLOCKS_PATH,
+                file_dir,
+                file_name.replace(".py", ""),
+                f"func_num_{entry_num}_original_" + file_name,
             )
-            save_path_block = (
-                BLOCKS_PATH
-                + file_dir
-                + "/"
-                + f"func_num_{entry_num}"
-                + entry["file_path"].split("/")[-1].split(".")[0]
-                + "_block.py"
+            save_path_block = os.path.join(
+                BLOCKS_PATH,
+                file_dir,
+                file_name.replace(".py", ""),
+                f"func_num_{entry_num}_generated_" + file_name,
             )
 
             file_path = entry["file_path"]
@@ -36,6 +40,8 @@ for data in tqdm.tqdm(results_data):
             model_output = entry["model_output"]
 
             with open(save_path, "w") as file:
+                # remove the the empty scapces from the beginning of the prefix
+                prefix = prefix.lstrip()
                 file.write(prefix + suffix)
 
             with open(
@@ -43,8 +49,8 @@ for data in tqdm.tqdm(results_data):
                 "w",
             ) as file:
                 # remove prefix from model_output
-
-                file.write(model_output.replace(prefix, ""))
+                model_output = model_output.lstrip()
+                file.write(model_output)
     except Exception as e:
         print(e)
         error_count += 1
