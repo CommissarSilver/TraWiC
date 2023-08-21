2ec2bd1928c628d93f58d1068a0cc1d41f76e932
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-08-21 09:18:37 -0400

    MODIFIED inspector_test.py:
    running inference on the RandomForest model

--- src/inspector_test.py
+++ src/inspector_test.py
@@ -1,51 +1,55 @@
 import os
 import multiprocessing
 from tqdm import tqdm
-from data import comment_to_code_ratio
+
 import pandas as pd
+import pickle
+from sklearn.ensemble import RandomForestClassifier
 
-trained_files = []
-test_files = []
-valid_files = []
+# load the model
 
-
-def process_file(file_name):
-    try:
-        return (
-            file_name,
-            1
-            if 0.01
-            < comment_to_code_ratio(
-                os.path.join(os.getcwd(), "data", "the_stack", "python", file_name)
-            )
-            < 0.8
-            else 0,
+clf = pickle.load(open("/Users/ahura/Nexus/TWMC/rf_model.sav", "rb"))
+ds_s = pd.read_csv("/Users/ahura/Nexus/TWMC/all_test.csv")
+ds_s.drop(
+    columns=[
+        "class_nums_total",
+        "function_nums_total",
+        "variable_nums_total",
+        "string_nums_total",
+        "comment_nums_total",
+        "docstring_nums_total",
+    ],
+    inplace=True,
+)
+x, y = ds_s.iloc[:, 1:-1].values, ds_s.iloc[:, -1].values
+# calculate the accuracy, recall, precision, f1-score
+final_repo_result = {}
+final_repo_ground_truth = {}
+with open("/Users/ahura/Nexus/TWMC/inspector_test_detail.csv", "w") as f:
+    f.write("file_name,actual,predicted\n")
+    accuracy = 0
+    recall = 0
+    precision = 0
+    f1 = 0
+    for i in tqdm(range(len(x))):
+        f.write(f"{ds_s.iloc[i, 0]},{y[i]},{clf.predict(x[i].reshape(1, -1))[0]}\n")
+        repo_name = ds_s.iloc[i, 0].split("/")[0]
+        # if even one repo is predicted as 1, then the whole repo is predicted as 1
+        final_repo_result[repo_name] = (
+            final_repo_result.get(repo_name, 0) + clf.predict(x[i].reshape(1, -1))[0]
         )
-    except:
-        return (file_name, 2)
+        final_repo_ground_truth[repo_name] = y[i]
+        accuracy += y[i] == clf.predict(x[i].reshape(1, -1))[0]
+        recall += y[i] == clf.predict(x[i].reshape(1, -1))[0] and y[i] == 1
+        precision += (
+            y[i] == clf.predict(x[i].reshape(1, -1))[0]
+            and clf.predict(x[i].reshape(1, -1))[0] == 1
+        )
+    print(f"accuracy: {accuracy/len(x)}")
+    print(f"recall: {recall/len(x)}")
+    print(f"precision: {precision/len(x)}")
 
-
-if __name__ == "__main__":
-    # ignored_files = trained_files + test_files + valid_files
-    # all_files = [
-    #     i
-    #     for i in os.listdir(os.path.join(os.getcwd(), "data", "the_stack", "python"))
-    #     if i not in ignored_files
-    # ]
-
-    # with multiprocessing.Pool() as pool:
-    #     files_dict = dict(tqdm(pool.imap(process_file, all_files), total=len(all_files)))
-
-    # with open(os.path.join(os.getcwd(), "ds_python.csv"), "w") as f:
-    #     f.write("file_name,label\n")
-    #     for file_name, label in files_dict.items():
-    #         f.write(f"{file_name},{label}\n")
-    trained_ons = pd.read_csv("/Users/ahura/Nexus/TWMC/src/trained_on.csv")[
-        "file_name"
-    ].tolist()
-    entire_ds = pd.read_csv("/Users/ahura/Nexus/TWMC/ds_python.csv")
-    # drop the files that are already in the trained_on.csv
-    entire_ds = entire_ds[~entire_ds["file_name"].isin(trained_ons)]
-    # filter by the ones who have a 0 label
-    entire_ds = entire_ds[entire_ds["label"] == 0]
-    entire_ds.to_csv("/Users/ahura/Nexus/TWMC/src/ds_python_neg.csv", index=False)
+with open("/Users/ahura/Nexus/TWMC/inspector_test.csv", "w") as f:
+    f.write("repo_name,predicted,actual\n")
+    for k, v in final_repo_result.items():
+        f.write(f"{k},{1 if v>0 else 0},{final_repo_ground_truth[k]}\n")
