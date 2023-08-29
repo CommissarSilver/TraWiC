0905a2bd9b6ec9317aeaf249239336fc6e242285
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-08-29 10:55:47 -0400

    twicks to inspector_test to have more comprehensive results

--- src/inspector_test.py
+++ src/inspector_test.py
@@ -25,31 +25,80 @@ x, y = ds_s.iloc[:, 1:-1].values, ds_s.iloc[:, -1].values
 # calculate the accuracy, recall, precision, f1-score
 final_repo_result = {}
 final_repo_ground_truth = {}
-with open("/Users/ahura/Nexus/TWMC/inspector_test_detail.csv", "w") as f:
-    f.write("file_name,actual,predicted\n")
+
+final_repo_total = {}
+
+true_positives = 0
+false_positives = 0
+true_negatives = 0
+false_negatives = 0
+
+with open("/Users/ahura/Nexus/TWMC/inspector_test_repo_level_detail.csv", "w") as f:
+    f.write("repo_name,actual,predicted\n")
     accuracy = 0
     recall = 0
     precision = 0
     f1 = 0
     for i in tqdm(range(len(x))):
-        f.write(f"{ds_s.iloc[i, 0]},{y[i]},{clf.predict(x[i].reshape(1, -1))[0]}\n")
+        predicted_value = clf.predict(x[i].reshape(1, -1))[0]
+        actual_value = y[i]
+        if predicted_value == actual_value:
+            if actual_value == 1:
+                true_positives += 1
+            else:
+                true_negatives += 1
+        else:
+            if predicted_value == 1:
+                false_positives += 1
+            else:
+                false_negatives += 1
+
+        f.write(f"{ds_s.iloc[i, 0]},{actual_value,{predicted_value}}\n")
         repo_name = ds_s.iloc[i, 0].split("/")[0]
         # if even one repo is predicted as 1, then the whole repo is predicted as 1
         final_repo_result[repo_name] = (
-            final_repo_result.get(repo_name, 0) + clf.predict(x[i].reshape(1, -1))[0]
+            final_repo_result.get(repo_name, 0) + predicted_value
         )
-        final_repo_ground_truth[repo_name] = y[i]
-        accuracy += y[i] == clf.predict(x[i].reshape(1, -1))[0]
-        recall += y[i] == clf.predict(x[i].reshape(1, -1))[0] and y[i] == 1
-        precision += (
-            y[i] == clf.predict(x[i].reshape(1, -1))[0]
-            and clf.predict(x[i].reshape(1, -1))[0] == 1
-        )
+        final_repo_total[repo_name] = final_repo_total.get(repo_name, 0) + 1
+
+        final_repo_ground_truth[repo_name] = actual_value
+        accuracy += actual_value == predicted_value
+        recall += actual_value == predicted_value and actual_value == 1
+        precision += actual_value == predicted_value and predicted_value == 1
+
     print(f"accuracy: {accuracy/len(x)}")
     print(f"recall: {recall/len(x)}")
     print(f"precision: {precision/len(x)}")
+
+    print("True Positives: ", true_positives)
+    print("False Positives: ", false_positives)
+    print("True Negatives: ", true_negatives)
+    print("False Negatives: ", false_negatives)
 
 with open("/Users/ahura/Nexus/TWMC/inspector_test.csv", "w") as f:
     f.write("repo_name,predicted,actual\n")
     for k, v in final_repo_result.items():
         f.write(f"{k},{1 if v>0 else 0},{final_repo_ground_truth[k]}\n")
+
+repo_true_positives = 0
+repo_false_positives = 0
+repo_true_negatives = 0
+repo_false_negatives = 0
+accuracy = 0
+threshold = 0.4
+with open("/Users/ahura/Nexus/TWMC/inspector_test_repo_level.csv", "w") as f:
+    f.write("repo_name,predicted,actual\n")
+    for k, v in final_repo_result.items():
+        f.write(f"{k},{1 if v/final_repo_total[k]>threshold else 0},1\n")
+        if v / final_repo_total[k] > threshold:
+            repo_true_positives += 1
+        else:
+            repo_false_positives += 1
+        if v / final_repo_total[k] > threshold and final_repo_ground_truth[k] == 1:
+            accuracy += 1
+        if v / final_repo_total[k] > threshold and final_repo_ground_truth[k] == 0:
+            accuracy += 1
+
+print("accuracy: ", repo_true_positives / len(list(final_repo_total.keys())))
+print("Repo level true positives: ", repo_true_positives)
+print("Repo level false positives: ", repo_false_positives)
