1e994fc76b9112d9460d87890b3b9269cfb8c7c6
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-10-10 13:34:36 -0400

    MODIFIED inspector_train:
    _ Added grid search for finding the best rf model

--- src/inspector_train.py
+++ src/inspector_train.py
@@ -8,9 +8,10 @@ from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 syntactic_threshold = 100  # threshold for considering syntactic similarity
-semantic_threshold = 40  # threshold for considering semantic similarity
+semantic_threshold = 100  # threshold for considering semantic similarity
 
 combined_ds = pd.read_csv(
     os.path.join(
@@ -33,6 +34,13 @@ train_ds, test_ds = train_test_split(
 train_ds.drop(columns=["Unnamed: 0"], inplace=True)
 test_ds.drop(columns=["Unnamed: 0"], inplace=True)
 
+##### Grid Search #####
+param_grid = {
+    "n_estimators": [50, 100, 200],
+    "max_features": ["sqrt", "log2"],
+    "max_depth": [10, 20, 30, None],
+    "criterion": ["gini", "entropy"],
+}
 
 ##### Random Forest ####
 x, y = train_ds.iloc[:, :-1].values, train_ds.iloc[:, -1].values
@@ -41,8 +49,18 @@ print(f"Target shape: {y.shape}")
 print(f"Features Snippet: {x[:1]}")
 print(f"Target Snippet: {y[:1]}")
 
-clf = RandomForestClassifier(n_estimators=50, max_depth=100, random_state=0)
-clf.fit(x, y)
+clf = RandomForestClassifier()
+grid_search = GridSearchCV(
+    estimator=clf, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2, scoring="accuracy"
+)
+grid_search.fit(x, y)
+best_params = grid_search.best_params_
+best_score = grid_search.best_score_
+
+print(f"Best Parameters: {best_params}")
+print(f"Best Score: {best_score}")
+clf = grid_search.best_estimator_
+# clf.fit(x, y)
 ##### Feature Importance ####
 fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(111)
