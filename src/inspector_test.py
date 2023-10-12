6e425247a36ab03ba11ee0bc752025327cf9c5ee
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-10-12 11:18:26 -0400

    MODIFEID inspector_test:
    - Added the analysis part for different sem/syn thresholds
    - Added reporting precision, accuracy, and f-score reporting

--- src/inspector_test.py
+++ src/inspector_test.py
@@ -6,21 +6,26 @@ import pandas as pd
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 
-# load the model
-clf = pickle.load(open("/Users/ahura/Nexus/TWMC/rf_model.sav", "rb"))
+syntactic_threshold = 100  # threshold for considering syntactic similarity
+semantic_threshold = 80  # threshold for considering semantic similarity
 
-ds_s = pd.read_csv("/Users/ahura/Nexus/TWMC/all_test.csv")
-ds_s.drop(
-    columns=[
-        "class_nums_total",
-        "function_nums_total",
-        "variable_nums_total",
-        "string_nums_total",
-        "comment_nums_total",
-        "docstring_nums_total",
-    ],
-    inplace=True,
+# load the model
+clf = pickle.load(
+    open(
+        f"/Users/ahura/Nexus/TWMC/rf_model__syn{syntactic_threshold}_sem{semantic_threshold}.sav",
+        "rb",
+    )
 )
+
+ds_s = pd.read_csv(
+    os.path.join(
+        os.getcwd(),
+        "rf_data",
+        f"syn{syntactic_threshold}_sem{semantic_threshold}",
+        "train.csv",
+    )
+)
+
 x, y = ds_s.iloc[:, 1:-1].values, ds_s.iloc[:, -1].values
 # calculate the accuracy, recall, precision, f1-score
 final_repo_result = {}
@@ -33,7 +38,10 @@ false_positives = 0
 true_negatives = 0
 false_negatives = 0
 
-with open("/Users/ahura/Nexus/TWMC/inspector_test_per_script.csv", "w") as f:
+with open(
+    f"/Users/ahura/Nexus/TWMC/inspector_test_per_script__syn{syntactic_threshold}_sem{semantic_threshold}.csv",
+    "w",
+) as f:
     f.write("repo_name,actual,predicted\n")
     accuracy = 0
     recall = 0
@@ -75,7 +83,10 @@ with open("/Users/ahura/Nexus/TWMC/inspector_test_per_script.csv", "w") as f:
     print("True Negatives: ", true_negatives)
     print("False Negatives: ", false_negatives)
 
-with open("/Users/ahura/Nexus/TWMC/inspector_test_repo_min_thresh.csv", "w") as f:
+with open(
+    f"/Users/ahura/Nexus/TWMC/inspector_test_repo_min_thresh__syn{syntactic_threshold}_sem{semantic_threshold}.csv",
+    "w",
+) as f:
     f.write("repo_name,predicted,actual\n")
     for k, v in final_repo_result.items():
         f.write(f"{k},{1 if v>0 else 0},{final_repo_ground_truth[k]}\n")
@@ -85,22 +96,34 @@ repo_false_positives = 0
 repo_true_negatives = 0
 repo_false_negatives = 0
 accuracy = 0
-threshold = 0.4  # if more than 40% of the files in a repo are predicted as 1, then the whole repo is predicted as 1
+threshold = 0.6  # if more than 40% of the files in a repo are predicted as 1, then the whole repo is predicted as 1
 with open(
-    f"/Users/ahura/Nexus/TWMC/inspector_test_repo_level_thresh_{threshold}.csv", "w"
+    f"/Users/ahura/Nexus/TWMC/inspector_test_repo_level_thresh_{threshold}__syn{syntactic_threshold}_sem{semantic_threshold}.csv",
+    "w",
 ) as f:
     f.write("repo_name,predicted,actual\n")
     for k, v in final_repo_result.items():
-        f.write(f"{k},{1 if v/final_repo_total[k]>threshold else 0},1\n")
-        if v / final_repo_total[k] > threshold:
+        predicted = 1 if v / final_repo_total[k] > threshold else 0
+        actual = final_repo_ground_truth[k]
+
+        f.write(f"{k},{predicted},{actual}\n")
+
+        # Calculate true positives, false positives, true negatives, and false negatives
+        if predicted == 1 and actual == 1:
             repo_true_positives += 1
-        else:
+        elif predicted == 1 and actual == 0:
             repo_false_positives += 1
-        if v / final_repo_total[k] > threshold and final_repo_ground_truth[k] == 1:
-            accuracy += 1
-        if v / final_repo_total[k] > threshold and final_repo_ground_truth[k] == 0:
-            accuracy += 1
+        elif predicted == 0 and actual == 0:
+            repo_true_negatives += 1
+        elif predicted == 0 and actual == 1:
+            repo_false_negatives += 1
+
 
 print("accuracy: ", repo_true_positives / len(list(final_repo_total.keys())))
-print("Repo level true positives: ", repo_true_positives)
-print("Repo level false positives: ", repo_false_positives)
+print("precision: ", repo_true_positives / (repo_true_positives + repo_false_positives))
+print(
+    "f-score: ",
+    2
+    * repo_true_positives
+    / (2 * repo_true_positives + repo_false_positives + repo_false_negatives),
+)
