a3e1cf6c8f327a51a1d4004894335c147cb1c30c
Vahid Majdinasab <vamaj@m4221-10.rdgi.polymtl.ca>
2023-11-05 08:56:43 -0500

    Update semantic threshold for inspector training and adjust visualization parameters

--- src/inspector_train.py
+++ src/inspector_train.py
@@ -15,7 +15,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 
 syntactic_threshold = 100  # threshold for considering syntactic similarity
-semantic_threshold = 80  # threshold for considering semantic similarity
+semantic_threshold = 40  # threshold for considering semantic similarity
 
 combined_ds = pd.read_csv(
     os.path.join(
@@ -64,19 +64,43 @@ best_score = grid_search.best_score_
 print(f"Best Parameters: {best_params}")
 print(f"Best Score: {best_score}")
 clf = grid_search.best_estimator_
-# clf.fit(x, y)
-##### Feature Importance ####
-fig = plt.figure(figsize=(10, 10))
-ax = fig.add_subplot(111)
+
+import seaborn as sns
+sns.set_theme(style="dark")
+
+fig, ax = plt.subplots(figsize=(12, 12))
+
+# Adjust font sizes
+ax.tick_params(labelsize=27)
+ax.set_xlabel("Importance", fontsize=20,fontdict={'weight': 'bold'})
+# ax.set_ylabel("Features", fontsize=10)
+
+# Horizontal bar chart with feature importances
 ax.barh(train_ds.columns[:-1], clf.feature_importances_)
-plt.savefig(
-    f"feature_importance__syn{syntactic_threshold}_sem{semantic_threshold}.png", dpi=300
-)
+
+# Rotate y-axis labels to fit
+plt.yticks(rotation=0)
+plt.tight_layout()
+# Save the figure with a descriptive filename
+plt.savefig(f"feature_importance__syn{syntactic_threshold}_sem{semantic_threshold}.png", dpi=300)
 
 #### Correlation Matrix ####
-fig = plt.figure(figsize=(10, 10))
-ax = fig.add_subplot(111)
-sns.heatmap(train_ds.corr(), annot=True, fmt=".2f", ax=ax)
+# Increase the figure size if you have many features.
+fig, ax = plt.subplots(figsize=(12, 12))
+
+# Create the heatmap, ensuring square cells and other configurations
+sns.heatmap(train_ds.corr(), annot=True, fmt=".2f", ax=ax, square=True)
+
+# Adjusting the Y-axis limit
+ax.set_ylim(len(train_ds.columns), 0)
+
+# Adjust the tick parameters if necessary
+ax.tick_params(labelsize=12)
+
+# Apply a tight layout with padding (if needed adjust the pad size)
+plt.tight_layout(pad=2)
+
+
 plt.savefig(
     f"correlation_matrix__syn{syntactic_threshold}_sem{semantic_threshold}.png", dpi=300
 )
@@ -104,7 +128,7 @@ tn, fp, fn, tp = confusion_matrix(
     clf.predict(test_ds.iloc[:, 1:].values),
 ).ravel()
 print(
-    f"True Negatives: {tn}, False Positives: {fp}, False Negatives: {fn}, True Positives: {tp}"
+    f"True Negatives: {tn/(tn+fp+fn+tp)}, False Positives: {fp/(tn+fp+fn+tp)}, False Negatives: {fn/(tn+fp+fn+tp)}, True Positives: {tp/(tn+fp+fn+tp)}"
 )
 # print the accuracy
 accuracy = accuracy_score(
