be844a0dae541e0e0cd157c80f29ac3cc7ba53d3
Vahid Majdinasab <vamaj@m4221-10.rdgi.polymtl.ca>
2023-11-05 15:01:35 -0500

    Update semantic threshold and file paths for inspector test

--- src/inspector_test.py
+++ src/inspector_test.py
@@ -7,12 +7,12 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 
 syntactic_threshold = 100  # threshold for considering syntactic similarity
-semantic_threshold = 80  # threshold for considering semantic similarity
+semantic_threshold = 60  # threshold for considering semantic similarity
 
 # load the model
 clf = pickle.load(
     open(
-        f"/Users/ahura/Nexus/TWMC/rf_model__syn{syntactic_threshold}_sem{semantic_threshold}.sav",
+        f"/store/travail/vamaj/TWMC/rf_model__syn{syntactic_threshold}_sem{semantic_threshold}.sav",
         "rb",
     )
 )
@@ -39,7 +39,7 @@ true_negatives = 0
 false_negatives = 0
 
 with open(
-    f"/Users/ahura/Nexus/TWMC/inspector_test_per_script__syn{syntactic_threshold}_sem{semantic_threshold}.csv",
+    f"/store/travail/vamaj/TWMC/inspector_test_per_script__syn{syntactic_threshold}_sem{semantic_threshold}.csv",
     "w",
 ) as f:
     f.write("repo_name,actual,predicted\n")
@@ -68,23 +68,19 @@ with open(
             final_repo_result.get(repo_name, 0) + predicted_value
         )
         final_repo_total[repo_name] = final_repo_total.get(repo_name, 0) + 1
-
         final_repo_ground_truth[repo_name] = actual_value
-        accuracy += actual_value == predicted_value
-        recall += actual_value == predicted_value and actual_value == 1
-        precision += actual_value == predicted_value and predicted_value == 1
 
-    print(f"accuracy: {accuracy/len(x)}")
-    print(f"recall: {recall/len(x)}")
-    print(f"precision: {precision/len(x)}")
+print('precision - file: ', true_positives / (true_positives + false_positives))
+print('accuracy - file: ', (true_positives + true_negatives) / len(x))
+print('f-score - file: ', 2 * true_positives / (2 * true_positives + false_positives + false_negatives))
 
-    print("True Positives: ", true_positives)
-    print("False Positives: ", false_positives)
-    print("True Negatives: ", true_negatives)
-    print("False Negatives: ", false_negatives)
+print("sensitivity - file: ", true_positives / (true_positives + false_negatives))
+print('specificity - file: ', true_negatives / (true_negatives + false_positives))
+
+print("*" * 100)
 
 with open(
-    f"/Users/ahura/Nexus/TWMC/inspector_test_repo_min_thresh__syn{syntactic_threshold}_sem{semantic_threshold}.csv",
+    f"/store/travail/vamaj/TWMC/inspector_test_repo_min_thresh__syn{syntactic_threshold}_sem{semantic_threshold}.csv",
     "w",
 ) as f:
     f.write("repo_name,predicted,actual\n")
@@ -96,9 +92,9 @@ repo_false_positives = 0
 repo_true_negatives = 0
 repo_false_negatives = 0
 accuracy = 0
-threshold = 0.6  # if more than 40% of the files in a repo are predicted as 1, then the whole repo is predicted as 1
+threshold = 0.4  # if more than 40% of the files in a repo are predicted as 1, then the whole repo is predicted as 1
 with open(
-    f"/Users/ahura/Nexus/TWMC/inspector_test_repo_level_thresh_{threshold}__syn{syntactic_threshold}_sem{semantic_threshold}.csv",
+    f"/store/travail/vamaj/TWMC/inspector_test_repo_level_thresh_{threshold}__syn{syntactic_threshold}_sem{semantic_threshold}.csv",
     "w",
 ) as f:
     f.write("repo_name,predicted,actual\n")
@@ -119,11 +115,59 @@ with open(
             repo_false_negatives += 1
 
 
-print("accuracy: ", repo_true_positives / len(list(final_repo_total.keys())))
-print("precision: ", repo_true_positives / (repo_true_positives + repo_false_positives))
 print(
-    "f-score: ",
+    "precision - 0.4: ",
+    repo_true_positives / (repo_true_positives + repo_false_positives),
+)
+print("accuracy - 0.4: ", (repo_true_positives + repo_true_negatives) / len(list(final_repo_total.keys())))
+print(
+    "f-score - 0.4: ",
     2
     * repo_true_positives
     / (2 * repo_true_positives + repo_false_positives + repo_false_negatives),
 )
+
+print('sensitivity - repo - 0.4: ', repo_true_positives / (repo_true_positives + repo_false_negatives))
+print('specificity - repo - 0.4: ', repo_true_negatives / (repo_true_negatives + repo_false_positives))
+
+print("*" * 100)
+
+repo_true_positives = 0
+repo_false_positives = 0
+repo_true_negatives = 0
+repo_false_negatives = 0
+
+threshold = 0.6  # if more than 40% of the files in a repo are predicted as 1, then the whole repo is predicted as 1
+with open(
+    f"/store/travail/vamaj/TWMC/inspector_test_repo_level_thresh_{threshold}__syn{syntactic_threshold}_sem{semantic_threshold}.csv",
+    "w",
+) as f:
+    f.write("repo_name,predicted,actual\n")
+    for k, v in final_repo_result.items():
+        predicted = 1 if v / final_repo_total[k] > threshold else 0
+        actual = final_repo_ground_truth[k]
+
+        f.write(f"{k},{predicted},{actual}\n")
+
+        # Calculate true positives, false positives, true negatives, and false negatives
+        if predicted == 1 and actual == 1:
+            repo_true_positives += 1
+        elif predicted == 1 and actual == 0:
+            repo_false_positives += 1
+        elif predicted == 0 and actual == 0:
+            repo_true_negatives += 1
+        elif predicted == 0 and actual == 1:
+            repo_false_negatives += 1
+print(
+    "precision - 0.6: ",
+    repo_true_positives / (repo_true_positives + repo_false_positives),
+)
+print("accuracy - 0.6: ", (repo_true_positives +repo_true_negatives) / len(list(final_repo_total.keys())))
+print(
+    "f-score - 0.6: ",
+    2
+    * repo_true_positives
+    / (2 * repo_true_positives + repo_false_positives + repo_false_negatives),
+)
+print('sensitivity - repo - 0.6: ', repo_true_positives / (repo_true_positives + repo_false_negatives))
+print('specificity - repo - 0.6: ', repo_true_negatives / (repo_true_negatives + repo_false_positives))
