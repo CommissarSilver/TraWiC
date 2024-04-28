f71bb0e1e6b4f9eb663debb76012c536124e9ee4
CommissarSilver <a.majdinasab@hotmail.com>
2024-04-28 07:58:45 -0400

    prototype of the fine-tuning code

--- src/models/mistral.py
+++ src/models/mistral.py
@@ -1,8 +1,9 @@
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
-
+import torch
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
 model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
+device = "cpu"
 
 FIM_PREFIX = "<fim-prefix>"
 FIM_MIDDLE = "<fim-middle>"
@@ -10,16 +11,19 @@ FIM_SUFFIX = "<fim-suffix>"
 FIM_PAD = "<fim-pad>"
 EOD = "<|endoftext|>"
 
-tokenizer.add_special_tokens(
+num_added_special_tokens = tokenizer.add_special_tokens(
     {
-        "additional_special_tokens": [EOD, FIM_PREFIX, FIM_MIDDLE, FIM_SUFFIX, FIM_PAD],
-        "pad_token": EOD,
+        "additional_special_tokens": [FIM_PREFIX, FIM_MIDDLE, FIM_SUFFIX, FIM_PAD],
     }
 )
 
-device = "cpu"
 model.resize_token_embeddings(len(tokenizer))
+
 input_text = "<fim-prefix>def fib(n):<fim-suffix>    else:\n        return fib(n - 2) + fib(n - 1)<fim-middle>"
-inputs = tokenizer(input_text, return_tensors="pt", padding=True, return_token_type_ids=False).to(device)
-outputs = model.generate(inputs.input_ids, max_new_tokens=100)
+inputs = tokenizer.encode(
+    input_text,
+    return_tensors="pt",
+    return_token_type_ids=False,
+).to(device)
+outputs = model.generate(inputs, max_new_tokens=100)
 print(tokenizer.decode(outputs[0]))
