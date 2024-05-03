a6174ba74d579a95662b8f8b3ba7879ad6b53cba
CommissarSilver <a.majdinasab@hotmail.com>
2024-05-03 09:59:31 -0400

    refactor: Update file paths and remove redundant scripts

--- src/main_mistral.py
+++ src/main_mistral.py
@@ -14,7 +14,9 @@ from checker import Checker
 from models import MistralCoder
 
 # load logging configuration
-with open(os.path.join(os.getcwd(), "src", "logging_config.yaml"), "r") as f:
+with open(
+    os.path.join("/home/vamaj/scratch/TraWiC", "src", "logging_config.yaml"), "r"
+) as f:
     config = yaml.safe_load(f.read())
 
 logging.config.dictConfig(config)
@@ -29,7 +31,7 @@ parser.add_argument(
 parser.add_argument(
     "--dataset_path",
     type=str,
-    default="data",
+    default="data/repos",
     help="path to the dataset",
 )
 parser.add_argument(
@@ -53,20 +55,20 @@ parser.add_argument(
 parser.add_argument(
     "--run_num",
     type=str,
-    default="4",
+    default="1",
     help="run number",
 )
 parser.add_argument(
     "--working_dir",
     type=str,
-    default=os.getcwd(),
+    default="/home/vamaj/scratch/TraWiC",
     help="working directory",
 )
 
 args = parser.parse_args()
 
-WORKING_DIR = os.getcwd() if args.working_dir == os.getcwd() else args.working_dir
-print("\033[93m" + f"Working directory: {WORKING_DIR}" + "\033[0m")
+WORKING_DIR = args.working_dir
+print("\033[93m" + f"Working directory: {args.working_dir}" + "\033[0m")
 
 model = MistralCoder() if args.model == "mistral_coder" else None
 
@@ -100,7 +102,8 @@ def get_model_output(file_path):
         )
         if model_output == "too_many_tokens":
             f = open(
-                os.path.join(os.getcwd(), "run_results", "too_many_tokens.txt"), "a"
+                os.path.join(args.working_dir, "run_results", "too_many_tokens.txt"),
+                "a",
             )
             f.write(file_path + "\n")
             return None
@@ -136,7 +139,7 @@ def get_model_output(file_path):
                 return None
     with open(
         os.path.join(
-            os.getcwd(), "run_results", f"TokensRun{args.run_num}", "results.jsonl"
+            args.working_dir, "run_results", f"TokensRun{args.run_num}", "results.jsonl"
         ),
         "a",
     ) as f:
@@ -156,51 +159,63 @@ if __name__ == "__main__":
         logging.info("GPU is not available. Running on CPU")
 
     if not os.path.exists(
-        os.path.join(os.getcwd(), "run_results", f"TokensRun{args.run_num}")
+        os.path.join(args.working_dir, "run_results", f"TokensRun{args.run_num}")
     ):
-        os.mkdir(os.path.join(os.getcwd(), "run_results", f"TokensRun{args.run_num}"))
+        os.mkdir(
+            os.path.join(args.working_dir, "run_results", f"TokensRun{args.run_num}")
+        )
 
     dataset_files = []
     for dirpath, dirnames, filenames in os.walk(
-        os.path.join(WORKING_DIR, args.dataset_path)
+        os.path.join(args.working_dir, args.dataset_path)
     ):
         python_files = [file for file in filenames if file.endswith(".py")]
         if python_files:
             dataset_files.extend(
-                [os.path.join(WORKING_DIR, dirpath, file) for file in python_files]
+                [os.path.join(args.working_dir, dirpath, file) for file in python_files]
             )
 
     dataset_files = (
         sorted(dataset_files) if args.sorted else sorted(dataset_files, reverse=True)
     )
+    print(len(dataset_files))
+    try:
+        files_generated_blocks = open(
+            os.path.join(args.working_dir, "run_results", "generated.txt"), "r"
+        ).readlines()  # read already processed files
 
-    files_generated_blocks = open(
-        os.path.join(os.getcwd(), "run_results", "generated.txt"), "r"
-    ).readlines()  # read already processed files
+        files_generated_blocks = [file.rstrip("\n") for file in files_generated_blocks]
 
-    files_generated_blocks = [file.rstrip("\n") for file in files_generated_blocks]
+        files_generated_blocks = (
+            sorted(files_generated_blocks)
+            if args.sorted
+            else sorted(files_generated_blocks, reverse=True)
+        )
+    except Exception as e:
+        files_generated_blocks = []
 
-    files_generated_blocks = (
-        sorted(files_generated_blocks)
-        if args.sorted
-        else sorted(files_generated_blocks, reverse=True)
-    )
+    try:
+        already_processed_files = open(
+            os.path.join(args.working_dir, "run_results", "processed_tokens.txt"), "r"
+        ).readlines()  # read already processed files
+        already_processed_files = [
+            file.rstrip("\n") for file in already_processed_files
+        ]
+    except Exception as e:
+        already_processed_files = []
 
-    already_processed_files = open(
-        os.path.join(os.getcwd(), "run_results", "processed_tokens.txt"), "r"
-    ).readlines()  # read already processed files
-    already_processed_files = [file.rstrip("\n") for file in already_processed_files]
-
-    dangerous_files = open(
-        os.path.join(WORKING_DIR, "run_results", f"assert_errors.txt"),
-        "r",
-    ).readlines()
-    dangerous_files = [file.rstrip("\n") for file in dangerous_files]
+    try:
+        dangerous_files = open(
+            os.path.join(args.working_dir, "run_results", f"assert_errors.txt"),
+            "r",
+        ).readlines()
+        dangerous_files = [file.rstrip("\n") for file in dangerous_files]
+    except Exception as e:
+        dangerous_files = []
 
     for file_path in dataset_files:
         if (
-            file_path in files_generated_blocks
-            and file_path not in already_processed_files
+            file_path not in already_processed_files
             and file_path not in dangerous_files
         ):
             results = []
@@ -208,6 +223,7 @@ if __name__ == "__main__":
             result = get_model_output(file_path)
 
             with open(
-                os.path.join(WORKING_DIR, "run_results", "processed_tokens.txt"), "a"
+                os.path.join(args.working_dir, "run_results", "processed_tokens.txt"),
+                "a",
             ) as f:
                 f.write(file_path + "\n")
