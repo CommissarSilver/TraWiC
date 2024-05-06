86388eacfe063694e5d6ead1aa2402e2b9e03113
CommissarSilver <a.majdinasab@hotmail.com>
2024-05-06 08:48:13 -0400

    nothing important

--- download_model_local.py
+++ download_model_local.py
@@ -4,12 +4,12 @@ import os
 # Path to store the models
 base_dir = os.getcwd()
 
-# # Download Mistral with a unique cache directory
-# mistral_path = snapshot_download(
-#     repo_id="mistralai/Mistral-7B-v0.1",
-#     cache_dir=os.path.join(base_dir, "llms", "cache_mistral"),
-#     local_dir=os.path.join(base_dir, "llms", "mistral"),
-# )
+# Download Mistral with a unique cache directory
+mistral_path = snapshot_download(
+    repo_id="mistralai/Mistral-7B-v0.1",
+    cache_dir=os.path.join(base_dir, "llms", "cache_mistral"),
+    local_dir=os.path.join(base_dir, "llms", "mistral"),
+)
 
 # Download Llama with a unique cache directory
 llama_path = snapshot_download(
